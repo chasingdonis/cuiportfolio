ac29aac8f6689c03a19b812a944b719a5f5cb1f5
chasingdonis <cuixianning@hotmail.com>
2016-04-13 15:54:45 -0500

    week12: work-in-progress

---
+++ footer.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package cuiportfolio
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<ul class="icons">
+						<li><a href="https://www.linkedin.com/in/sidney-cui-6569185a"><img src="http://localhost:8888/php_final/wp-content/uploads/2016/04/icon_l.png"></a></li>
+						<li><a href="https://www.instagram.com/buildacanoe/"><img src="http://localhost:8888/php_final/wp-content/uploads/2016/04/icon_i.png"></a></li>
+                        						<li><a href="https://soundcloud.com/chasingdonis"><img src="http://localhost:8888/php_final/wp-content/uploads/2016/04/icon_s.png"></a></li>
+						<li><a href="mailto:flatlamb@gmail.com?Subject=Hello"><img src="http://localhost:8888/php_final/wp-content/uploads/2016/04/icon_mail.png"></a></li>
+					</ul>
+					<div class="copyright">
+						<p>Copyright&copy; 2016.  Xianning Cui <br>All Rights Reserved</p>
+					</div>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
